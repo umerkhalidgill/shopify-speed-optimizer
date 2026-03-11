749f57c8154bec3bd0dcece8a345fe5950bdfbc3
Umar Farooq <umerkhalidgill@gmail.com>
2026-03-11 14:17:46 -0400

    Update lazyload.js

--- scripts/lazyload.js
+++ scripts/lazyload.js
@@ -1,1 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
 
+  const lazyImages = document.querySelectorAll("img[data-src]");
+
+  const loadImage = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.removeAttribute("data-src");
+  };
+
+  const imageObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+
+      if (!entry.isIntersecting) {
+        return;
+      }
+
+      loadImage(entry.target);
+      observer.unobserve(entry.target);
+
+    });
+
+  });
+
+  lazyImages.forEach(image => {
+    imageObserver.observe(image);
+  });
+
+});
