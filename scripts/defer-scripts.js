a3efbb0d8490c7425e696376229a8e7a7465e3c5
Umar Farooq <umerkhalidgill@gmail.com>
2026-03-11 14:18:02 -0400

    Update defer-scripts.js

--- scripts/defer-scripts.js
+++ scripts/defer-scripts.js
@@ -1,1 +1,17 @@
 
+document.addEventListener("DOMContentLoaded", function () {
+
+  const scripts = document.querySelectorAll("script[data-defer]");
+
+  scripts.forEach(script => {
+
+    const newScript = document.createElement("script");
+
+    newScript.src = script.getAttribute("data-src");
+    newScript.defer = true;
+
+    document.body.appendChild(newScript);
+
+  });
+
+});
